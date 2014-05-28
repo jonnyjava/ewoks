09eb2dddc98977863821931c692d650b4ce8b899
Vicente <vpons@norauto.es>
2014-05-28 18:00:29 +0200

    Added relation: garage has many fees.

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -1,3 +1,4 @@
 class Garage < ActiveRecord::Base
   has_many :holidays
+  has_many :fees
 end
--- spec/models/garage_spec.rb
+++ spec/models/garage_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Garage do
   it { should have_many(:holidays) }
+  it { should have_many(:fees) }
 end
