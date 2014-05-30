a1fda1d5a409b5732877bbc213d2ac639ba0b032
Andrea <jonnyjava.net@gmail.com>
2014-05-30 12:44:05 +0200

    basic user authorization check

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,16 @@
 class HomeController < ApplicationController
-  before_filter :authenticate_user!, :only => :already_registered
+  before_filter :authenticate_user!, only: :already_registered
+  skip_authorization_check
 
   def index
     render  'home/index'
   end
 
   def already_registered
-    render :layout => 'application'
+    render layout: 'application'
+  end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  Roles = [ :admin , :country_manager, :owner ]
+
+  def is?( requested_role )
+    self.role == requested_role.to_s
+  end
 end
