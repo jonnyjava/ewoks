3fb9c6b6e9930aadda040c7217514a671a253f07
Andrea <jonnyjava.net@gmail.com>
2014-06-02 12:26:38 +0200

    appling roles to users views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  Roles = [ :admin , :country_manager, :owner ]
+  ADMIN = 0
+  COUNTRY_MANAGER = 1
+  OWNER = 2
+  ROLES = {admin: ADMIN, country_manager: COUNTRY_MANAGER, owner: OWNER}
 
   def is?( requested_role )
-    self.role == requested_role.to_s
+    self.role == ROLES[requested_role]
   end
 end
