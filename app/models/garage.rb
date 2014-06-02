37e2462e99832c63e144d897f17407e8aa4a012e
Andrea <jonnyjava.net@gmail.com>
2014-06-02 17:13:08 +0200

    added relation between user and garage

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -1,4 +1,5 @@
 class Garage < ActiveRecord::Base
+  belongs_to :user, foreign_key: "owner_id"
   has_many :holidays
   has_many :fees
   has_and_belongs_to_many :properties
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :garage, foreign_key: "owner_id"
+
   ADMIN = 0
   COUNTRY_MANAGER = 1
   OWNER = 2
