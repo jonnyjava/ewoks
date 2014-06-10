9cbae0b65efebef2f3885772cf22df8323e118a5
Vicente <vpons@norauto.es>
2014-06-10 10:01:57 +0200

    Added test to prove the relationships of GarageProperty.

--- spec/models/garage_property_spec.rb
+++ spec/models/garage_property_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe GarageProperty do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:garage) }
+  it { should belong_to(:property) }
 end
