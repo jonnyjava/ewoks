31943b69a88fdb5c066c6663698a3d8ccb97bd2c
Vicente <vpons@norauto.es>
2014-06-12 11:08:05 +0200

    Create migration to add enable field in Garage.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140611174649) do
+ActiveRecord::Schema.define(version: 20140612090247) do
 
   create_table "fees", force: true do |t|
     t.string   "name"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20140611174649) do
     t.datetime "updated_at"
     t.string   "country"
     t.integer  "owner_id"
+    t.boolean  "enable"
   end
 
   create_table "holidays", force: true do |t|
