b8c7888c66ec9b8a80dea656cab38c85bedb118a
Andrea <jonnyjava.net@gmail.com>
2014-06-19 15:24:43 +0200

    fixed user factory, password generation too short

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   factory :user do
     email "#{Faker::Internet::email}"
-    password "#{Faker::Internet::password}"
+    password "#{Faker::Internet::password(10)}"
 
     factory :admin, class: User do
       role User::ADMIN
