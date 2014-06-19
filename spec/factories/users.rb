4cd5b36ed418eea9dbd75ffd3ad83b6f5ecc1533
Andrea <jonnyjava.net@gmail.com>
2014-06-19 10:35:44 +0200

    creating and using new factories for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,17 @@ FactoryGirl.define do
   factory :user do
     email "#{Faker::Internet::email}"
     password "#{Faker::Internet::password}"
+
+    factory :admin, class: User do
+      role User::ADMIN
+    end
+
+    factory :country_manager, class: User do
+      role User::COUNTRY_MANAGER
+    end
+
+    factory :owner, class: User do
+      role User::OWNER
+    end
   end
 end
--- spec/support/request_macros.rb
+++ spec/support/request_macros.rb
@@ -1,7 +1,7 @@
 module RequestMacros
   def login_user
     before(:each) do
-      user = FactoryGirl.create(:user)
+      user = FactoryGirl.create(:admin)
       post_via_redirect user_session_path, 'user[email]' => user.email, 'user[password]' => user.password
     end
   end
