e0d28872400c8af745c91b8ac60e0b9807be0524
Andrea <jonnyjava.net@gmail.com>
2014-06-19 10:36:16 +0200

    adding roles for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :garage, foreign_key: "owner_id"
+  enum role: [:admin, :country_manager, :owner]
+
+  ADMIN = roles[:admin]
+  COUNTRY_MANAGER = roles[:country_manager]
+  OWNER = roles[:owner]
+
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :owner
+  end
 end
