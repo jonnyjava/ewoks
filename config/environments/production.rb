30210adbdc0062e4cd73c278bc5f28b7e18245d7
Andrea <jonnyjava.net@gmail.com>
2014-06-23 15:26:03 +0200

    assets compilation for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,12 +65,21 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
+# Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
-  # Send deprecation notices to registered listeners.
+  # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Compress JavaScript and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline
+  config.assets.compile = true
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
