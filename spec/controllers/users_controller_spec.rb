4057e7afa5967dde7a1172a27d187f2e9222a20b
Vicente <vpons@norauto.es>
2014-06-25 09:50:56 +0200

    Fix good practices recommended for hound.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,8 +24,8 @@ describe UsersController do
   # This should return the minimal set of attributes required to create a valid
   # User. As you add validations to User, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "email" => "#{Faker::Internet::email}", "password" => "#{Faker::Internet::password(10)}" } }
-  let(:invalid_attributes) { { "wrong_param" => "wrong" } }
+  let(:valid_attributes) { { 'email' => "#{Faker::Internet::email}", "password" => "#{Faker::Internet::password(10)}" } }
+  let(:invalid_attributes) { { 'wrong_param' => 'wrong' } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
