da68956ab9f808cd2ca898148841953ec94373b1
Andrea <jonnyjava.net@gmail.com>
2014-06-26 12:28:32 +0200

    linking fees to its garage to show them in its sheet

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -2,4 +2,8 @@ class Fee < ActiveRecord::Base
   belongs_to :garage
   validates :name, presence: true
   validates :price, presence: true
+
+  def tyre_fee
+    TyreFee.where(fee_id: id).first
+  end
 end
