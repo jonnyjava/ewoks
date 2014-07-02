93f84c45fd892b45a2855e559fc79211548d61bd
GpoNsU <simpons@gmail.com>
2014-07-02 15:15:31 +0200

    Create and edit timetables.

--- app/controllers/timetables_controller.rb
+++ app/controllers/timetables_controller.rb
@@ -1,13 +1,62 @@
 class TimetablesController < ApplicationController
   before_action :set_garage
+  before_action :set_timetable, only: [:show, :edit, :update, :destroy]
 
   def index
     @timetables = @garage.timetables
   end
 
+  def show
+  end
+
+  def new
+    @timetable = @garage.timetables.build
+  end
+
+  def edit
+  end
+
+  def create
+    @timetable = Timetable.new(timetable_params)
+
+    respond_to do |format|
+      if @timetable.save
+        format.html { redirect_to garage_timetables_url(@garage), notice: 'Timetable was successfully created.' }
+        format.json { render :show, status: :created, location: @timetable }
+      else
+        format.html { render :new }
+        format.json { render json: @timetable.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @timetable.update(timetable_params)
+        format.html { redirect_to garage_timetables_url(@garage), notice: 'Timetable was successfully updated.' }
+        format.json { render :show, status: :ok, location: @timetable }
+      else
+        format.html { render :edit }
+        format.json { render json: @timetable.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @timetable.destroy
+    respond_to do |format|
+      format.html { redirect_to garage_timetables_url(@garage), notice: 'Timetable was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
     def set_garage
       @garage = Garage.find(params[:garage_id])
+    end
+
+    def set_timetable
+      @timetable = Timetable.find(params[:id])
     end
 
     def timetable_params
---
+++ app/views/timetables/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @timetable, :id, :created_at, :updated_at
