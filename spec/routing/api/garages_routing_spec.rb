7a1f2ca1549df71f2f40c182a826d3e84215816b
Andrea <jonnyjava.net@gmail.com>
2014-07-14 15:09:01 +0200

    refactoring api routing test

--- spec/routing/api/garages_routing_spec.rb
+++ spec/routing/api/garages_routing_spec.rb
@@ -4,7 +4,7 @@ describe API::GaragesController do
   describe "routing" do
 
     it "routes to #index" do
-      get("http://api.localhost.dev/garages").should route_to("api/garages#index", subdomain: 'api')
+      api_get('garages').should route_to("api/garages#index", subdomain: 'api')
     end
 
   end
