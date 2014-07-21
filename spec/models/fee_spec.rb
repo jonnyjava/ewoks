7bc92e5194d35a779f8e405371874d3b22f0ac5f
GpoNsU <simpons@gmail.com>
2014-07-21 12:04:17 +0200

    Change test fees model to bugfix.

--- spec/models/fee_spec.rb
+++ spec/models/fee_spec.rb
@@ -4,5 +4,5 @@ describe Fee do
   it { should belong_to(:garage) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:price) }
-  it { should have_db_column(:price).with_options(precision: 2) }
+  it { should have_db_column(:price).with_options(precision: 4, scale: 2) }
 end
