cd6c24ed79221a93fdf5dcf3dae50383d73a49e2
GpoNsU <simpons@gmail.com>
2014-08-04 14:54:13 +0200

    Added translations to mailers.

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -7,7 +7,7 @@ describe UserMailer do
       sent_email = UserMailer.send_generated_password(owner)
       sent_email.should deliver_from(EMAIL_ADMIN)
       sent_email.should deliver_to(owner.email)
-      sent_email.should have_subject('Your login for Ewoks')
+      sent_email.should have_subject( I18n.t('Your login for Ewoks') )
       sent_email.should have_body_text(/Your account has been confirmed!/)
       sent_email.should have_body_text(/#{owner.email}/)
       sent_email.should have_body_text(/#{owner.password}/)
