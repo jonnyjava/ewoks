cc485705ef66be461d3516424f15e6aa3ca35d0f
GpoNsU <simpons@gmail.com>
2014-08-04 10:35:33 +0200

    EWOKS-1 #comment Added user mailer to send activation notification.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,4 +5,9 @@ class UserMailer < ActionMailer::Base
     @user = user
     mail(to: user.email, subject: 'Your login for Ewoks')
   end
+
+  def send_activation_notification(user)
+    @user = user
+    mail(to: user.email, subject: 'Your account has been activated')
+  end
 end
