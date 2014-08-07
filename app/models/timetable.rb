538710e76a1efbd871006ef4d7d2233ca8db2c6d
GpoNsU <simpons@gmail.com>
2014-08-07 15:30:27 +0200

    Added timetable validators.

--- app/models/timetable.rb
+++ app/models/timetable.rb
@@ -1,5 +1,6 @@
 class Timetable < ActiveRecord::Base
   belongs_to :garage
+  validates_with TimetableValidator
 
   def closes_at_noon(day_name)
     send("#{day_name.downcase[0, 3]}_morning_close".to_sym).blank?
