d3fabba01b1eecac34546c4a3fb10a2ab5b32598
Andrea <jonnyjava.net@gmail.com>
2014-08-08 15:21:39 +0200

    dateformat initializer

---
+++ config/initializers/date_format.rb
@@ -0,0 +1,12 @@
+# Date
+#Date::DATE_FORMATS[:default] = "%d-%m-%Y"
+Date::DATE_FORMATS[:default] = "%e %b %Y"
+
+# DateTime
+
+#DateTime::DATE_FORMATS[:default] = "%d-%m-%Y"
+DateTime::DATE_FORMATS[:default] = "%e %b %Y"
+
+# Time
+#Time::DATE_FORMATS[:default] = "%d-%m-%Y %H:%M:%S"
+Time::DATE_FORMATS[:default] = "%a, %d %b %Y %H:%M:%S %z"
