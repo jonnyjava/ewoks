1a10884d04e61e675496ef5466b73c29a88ace91
Andrea <jonnyjava.net@gmail.com>
2014-08-11 17:02:56 +0200

    DRYing controllers refactoring shared code into a concern

--- app/controllers/public_form_controller.rb
+++ app/controllers/public_form_controller.rb
@@ -1,32 +1,11 @@
 class PublicFormController < ApplicationController
+  include PermittedParametersForPublicControllers
+
   skip_before_filter :authenticate_user!
 
   def public_form
     @garage = Garage.new(country: nil)
     render :public_form
-  end
-
-  def create_extra_holidays(rows)
-    holidays = []
-    (1..rows).each do |i|
-      next unless params["holiday_#{i}".to_sym].present?
-      holidays << Holiday.new(extra_holiday_params(i))
-    end
-    holidays
-  end
-
-  def validate_all(garage_properties)
-    valid = true
-    garage_properties.each do |property|
-      valid = property.valid? && valid
-    end
-    valid
-  end
-
-  def assign_garage_to_his_related(garage, garage_properties)
-    garage_properties.each do |property|
-      property.garage = garage if property.respond_to?(:garage_id)
-    end
   end
 
   def create
@@ -75,27 +54,30 @@ class PublicFormController < ApplicationController
 
   private
 
-  def garage_params
-    params.require(:garage).permit(:name, :owner, :country, :street, :zip, :city, :email, :phone, :mobile, :fax, :latitude, :longitude, :tax_id, :website)
+  def create_extra_holidays(rows)
+    holidays = []
+    (1..rows).each do |i|
+      next unless params["holiday_#{i}".to_sym].present?
+      holidays << Holiday.new(extra_holiday_params(i))
+    end
+    holidays
   end
 
-  def timetable_params
-    params.require(:timetable).permit(:mon_morning_open, :mon_morning_close, :mon_afternoon_open, :mon_afternoon_close, :tue_morning_open, :tue_morning_close, :tue_afternoon_open, :tue_afternoon_close, :wed_morning_open, :wed_morning_close, :wed_afternoon_open, :wed_afternoon_close, :thu_morning_open, :thu_morning_close, :thu_afternoon_open, :thu_afternoon_close, :fri_morning_open, :fri_morning_close, :fri_afternoon_open, :fri_afternoon_close, :sat_morning_open, :sat_morning_close, :sat_afternoon_open, :sat_afternoon_close, :sun_morning_open, :sun_morning_close, :sun_afternoon_open, :sun_afternoon_close, :garage_id)
+  def validate_all(garage_properties)
+    valid = true
+    garage_properties.each do |property|
+      valid = property.valid? && valid
+    end
+    valid
   end
 
-  def holiday_params
-    params.require(:holiday).permit(:name, :start_date, :end_date)
+  def assign_garage_to_his_related(garage, garage_properties)
+    garage_properties.each do |property|
+      property.garage = garage if property.respond_to?(:garage_id)
+    end
   end
 
   def extra_holiday_params(i)
     params.require("holiday_#{i}".to_sym).permit(:name, :start_date, :end_date)
-  end
-
-  def tyre_fee_params
-    params.require(:tyre_fee).permit(:vehicle_type, :diameter_min, :diameter_max, :rim_type)
-  end
-
-  def fee_params
-    params.require(:fee).permit(:name, :price)
   end
 end
--- app/controllers/public_wizard_controller.rb
+++ app/controllers/public_wizard_controller.rb
@@ -1,7 +1,7 @@
 class PublicWizardController < ApplicationController
+  include PermittedParametersForPublicControllers
   layout 'public_form'
   before_action :set_garage, except: [:public_wizard, :create_garage]
-
   skip_before_filter :authenticate_user!
 
   def public_wizard
@@ -81,25 +81,5 @@ class PublicWizardController < ApplicationController
 
   def set_garage
     @garage = Garage.find(params[:garage_id])
-  end
-
-  def garage_params
-    params.require(:garage).permit(:name, :owner, :country, :street, :zip, :city, :email, :phone, :mobile, :fax, :latitude, :longitude, :tax_id, :website)
-  end
-
-  def timetable_params
-    params.require(:timetable).permit(:garage_id, :mon_morning_open, :mon_morning_close, :mon_afternoon_open, :mon_afternoon_close, :tue_morning_open, :tue_morning_close, :tue_afternoon_open, :tue_afternoon_close, :wed_morning_open, :wed_morning_close, :wed_afternoon_open, :wed_afternoon_close, :thu_morning_open, :thu_morning_close, :thu_afternoon_open, :thu_afternoon_close, :fri_morning_open, :fri_morning_close, :fri_afternoon_open, :fri_afternoon_close, :sat_morning_open, :sat_morning_close, :sat_afternoon_open, :sat_afternoon_close, :sun_morning_open, :sun_morning_close, :sun_afternoon_open, :sun_afternoon_close, :garage_id)
-  end
-
-  def holiday_params
-    params.require(:holiday).permit(:garage_id, :name, :start_date, :end_date)
-  end
-
-  def tyre_fee_params
-    params.require(:tyre_fee).permit(:vehicle_type, :diameter_min, :diameter_max, :rim_type)
-  end
-
-  def fee_params
-    params.require(:fee).permit(:name, :price)
   end
 end
