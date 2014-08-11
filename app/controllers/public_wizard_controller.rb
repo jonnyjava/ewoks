41f35d6f29b85d3b87611af4052886ba7282fa73
Andrea <jonnyjava.net@gmail.com>
2014-08-11 11:54:58 +0200

    wizard, step 1, create garage

--- app/controllers/public_wizard_controller.rb
+++ app/controllers/public_wizard_controller.rb
@@ -3,6 +3,25 @@ class PublicWizardController < ApplicationController
 
   def public_wizard
     @garage = Garage.new(country: nil)
-    render :public_wizard
+    render :public_wizard, layout: "public_form"
+  end
+
+  def create_garage
+    @garage = Garage.new(garage_params)
+    @garage.timetable = Timetable.new
+
+    respond_to do |format|
+      if @garage.save
+        format.html { redirect_to create_timetable_url(@garage), notice: 'Garage was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def garage_params
+    params.require(:garage).permit(:name, :owner, :country, :street, :zip, :city, :email, :phone, :mobile, :fax, :latitude, :longitude, :tax_id, :website)
   end
 end
