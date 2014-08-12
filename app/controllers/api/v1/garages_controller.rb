3178c107937df93a764af00f07ea067faa987839
GpoNsU <simpons@gmail.com>
2014-08-12 12:41:06 +0200

    Refactored index api.

--- app/controllers/api/v1/garages_controller.rb
+++ app/controllers/api/v1/garages_controller.rb
@@ -3,26 +3,17 @@ module API
     class GaragesController < ApplicationController
       skip_before_filter :authenticate_user!
       before_action :authenticate
+      before_action :set_country, :set_location, only: :index
 
       def index
         garages = Garage.active
-        country = params[:country]
-        city = params[:city]
-        zip = params[:zip]
-        radius = params[:radius]
-        price = params[:tyre_fee]
 
-        country = @current_user.country unless country
+        garages = filtered_by_price(
+                  garages, params[:tyre_fee]) if params[:tyre_fee]
+        garages = filtered_by_radius(
+                  garages, params[:radius]) if params[:radius]
+        garages = filtered_by_default(garages, params) unless params[:radius]
 
-        if radius
-          location = [city, zip, country].compact.join(', ')
-          garages = garages.find_by_radius_from_location(location, radius)
-        else
-          garages = garages.by_country(country)
-          garages = garages.by_city(city) if city
-          garages = garages.by_zip(zip) if zip
-        end
-        garages = garages.with_tyre_fee_less_than(price) if price
         @garages = garages
       end
 
@@ -31,21 +22,45 @@ module API
         @holidays = @garage.holidays.all
       end
 
-    protected
+      protected
 
       def authenticate
         authenticate_token || render_unauthorized
       end
 
       def authenticate_token
-        authenticate_with_http_token do |token, options|
+        authenticate_with_http_token do |token|
           @current_user = User.find_by_auth_token(token)
         end
       end
 
       def render_unauthorized
-        self.headers['WWW-Authenticate'] = 'Token realm="Application"'
+        headers['WWW-Authenticate'] = 'Token realm="Application"'
         render json: 'Bad credentials', status: 401
+      end
+
+      def filtered_by_radius(garages, radius)
+        garages.find_by_radius_from_location(@location, radius)
+      end
+
+      def filtered_by_price(garages, price)
+        garages.with_tyre_fee_less_than(price)
+      end
+
+      def filtered_by_default(garages, params)
+        garages = garages.by_zip(params[:zip]) if params[:zip]
+        garages = garages.by_city(params[:city]) if params[:city]
+        garages.by_country(@country)
+      end
+
+      private
+
+      def set_country
+        @country = params[:country] || @current_user.country
+      end
+
+      def set_location
+        @location = [params[:city], params[:zip], @country].compact.join(', ')
       end
     end
   end
