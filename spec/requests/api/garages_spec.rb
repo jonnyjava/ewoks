ebb27ca60c02f49c314943d13a7868058be3739e
GpoNsU <simpons@gmail.com>
2014-08-12 15:04:16 +0200

    Rubocop fixes.

--- spec/requests/api/garages_spec.rb
+++ spec/requests/api/garages_spec.rb
@@ -6,56 +6,74 @@ describe 'Garages' do
     ActionController::HttpAuthentication::Token.encode_credentials(token)
   end
 
-
   let!(:user) { FactoryGirl.create(:user) }
   describe 'index' do
     it 'should return status 200' do
-      api_get 'garages.json', {}, { 'Authorization' => token_header(user.auth_token) }
+      api_get 'garages.json', {},
+              'Authorization' => token_header(user.auth_token)
       response.status.should be(200)
       response.body.should_not be_empty
       response.content_type.should == Mime::JSON
     end
 
     it 'should return status 404' do
-      api_get 'alderaan.json', {}, { 'Authorization' => token_header(user.auth_token) }
+      api_get 'alderaan.json', {},
+              'Authorization' => token_header(user.auth_token)
       response.status.should be(404)
       response.body.should_not be_empty
       response.content_type.should == Mime::JSON
     end
 
     it 'should return status 401' do
-      api_get 'garages.json', {}, { 'Authorization' => "Token token=wrongtoken"}
+      api_get 'garages.json', {},
+              'Authorization' => 'Token token=wrongtoken'
       response.status.should be(401)
     end
 
     context 'with querystring' do
-      let!(:spanish_garage) { FactoryGirl.create(:garage, country: 'Spain', zip: '00000', city: 'Valencia') }
-      let!(:french_garage) { FactoryGirl.create(:garage, country: 'France', zip: '111111', city: 'Marseille') }
-      let(:spanish_fee) { FactoryGirl.create(:fee, garage: spanish_garage, price: 20) }
-      let(:french_fee) { FactoryGirl.create(:fee, garage: french_garage, price: 50) }
+      let!(:spanish_garage) do
+        FactoryGirl
+        .create(:garage, country: 'Spain', zip: '00000', city: 'Valencia')
+      end
+
+      let!(:french_garage) do
+        FactoryGirl
+        .create(:garage, country: 'France', zip: '111111', city: 'Marseille')
+      end
+
+      let(:spanish_fee) do
+        FactoryGirl.create(:fee, garage: spanish_garage, price: 20)
+      end
+
+      let(:french_fee) do
+        FactoryGirl.create(:fee, garage: french_garage, price: 50)
+      end
 
       it 'should filter by country' do
-        api_get 'garages.json?country=Spain', {}, { 'Authorization' => token_header(user.auth_token) }
+        api_get 'garages.json?country=Spain', {},
+                'Authorization' => token_header(user.auth_token)
         response.status.should be(200)
-        ids = json(response.body).collect { |g| g[:id] }
+        ids = json(response.body).map { |g| g[:id] }
         ids.count.should be(1)
         ids.should include(spanish_garage.id)
         ids.should_not include(french_garage.id)
       end
 
       it 'should filter by zip' do
-        api_get 'garages.json?zip=00000', {}, { 'Authorization' => token_header(user.auth_token) }
+        api_get 'garages.json?zip=00000', {},
+                'Authorization' => token_header(user.auth_token)
         response.status.should be(200)
-        ids = json(response.body).collect { |g| g[:id] }
+        ids = json(response.body).map { |g| g[:id] }
         ids.count.should be(1)
         ids.should include(spanish_garage.id)
         ids.should_not include(french_garage.id)
       end
 
       it 'should filter by city' do
-        api_get 'garages.json?city=Valencia', {}, { 'Authorization' => token_header(user.auth_token) }
+        api_get 'garages.json?city=Valencia', {},
+                'Authorization' => token_header(user.auth_token)
         response.status.should be(200)
-        ids = json(response.body).collect { |g| g[:id] }
+        ids = json(response.body).map { |g| g[:id] }
         ids.count.should be(1)
         ids.should include(spanish_garage.id)
         ids.should_not include(french_garage.id)
@@ -64,23 +82,26 @@ describe 'Garages' do
       it 'should filter by tyre_fee' do
         spanish_fee
         french_fee
-        api_get 'garages.json?price=25', {}, { 'Authorization' => token_header(user.auth_token) }
+        api_get 'garages.json?price=25', {},
+                'Authorization' => token_header(user.auth_token)
         response.status.should be(200)
-        ids = json(response.body).collect { |g| g[:id] }
+        ids = json(response.body).map { |g| g[:id] }
         ids.count.should be(1)
         ids.should include(spanish_garage.id)
         ids.should_not include(french_garage.id)
       end
 
       it 'should filter by country, zip and tyre_fee' do
-        another_garage = FactoryGirl.create(:garage, country: 'Spain', zip: '00000')
+        another_garage = FactoryGirl
+                         .create(:garage, country: 'Spain', zip: '00000')
         FactoryGirl.create(:fee, garage: another_garage, price: 40)
         spanish_fee
         french_fee
 
-        api_get 'garages.json?country=Spain&zip=00000&price=25', {}, { 'Authorization' => token_header(user.auth_token) }
+        api_get 'garages.json?country=Spain&zip=00000&price=25', {},
+                'Authorization' => token_header(user.auth_token)
         response.status.should be(200)
-        ids = json(response.body).collect { |g| g[:id] }
+        ids = json(response.body).map { |g| g[:id] }
         ids.count.should be(1)
         ids.should include(spanish_garage.id)
         ids.should_not include(french_garage.id)
@@ -96,18 +117,20 @@ describe 'Garages' do
       end
 
       it 'should return one garage searching by city' do
-        api_get 'garages.json?country=Italy&city=Torino&radius=20', {}, { 'Authorization' => token_header(user.auth_token) }
+        api_get 'garages.json?country=Italy&city=Torino&radius=20', {},
+                'Authorization' => token_header(user.auth_token)
         response.status.should be(200)
-        ids = json(response.body).collect { |g| g[:id] }
+        ids = json(response.body).map { |g| g[:id] }
         ids.count.should be(1)
         ids.should include(garage_inside_radius.id)
         ids.should_not include(garage_outside_radius.id)
       end
 
       it 'should return one garage searching by zip' do
-        api_get 'garages.json?country=Italy&city=10139&radius=20', {}, { 'Authorization' => token_header(user.auth_token) }
+        api_get 'garages.json?country=Italy&city=10139&radius=20', {},
+                'Authorization' => token_header(user.auth_token)
         response.status.should be(200)
-        ids = json(response.body).collect { |g| g[:id] }
+        ids = json(response.body).map { |g| g[:id] }
         ids.count.should be(1)
         ids.should include(garage_inside_radius.id)
         ids.should_not include(garage_outside_radius.id)
