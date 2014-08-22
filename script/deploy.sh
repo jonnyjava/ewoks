fa99b83c1a06cedf8daccf995c328f93e27fbed6
GpoNsU <simpons@gmail.com>
2014-08-22 15:59:28 +0200

    Configure .gitignore to deploy.

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # Install dependencies
 # sudo apt-get install python-pip python-dev build-essential zip -y
 # sudo pip install awscli
@@ -8,9 +10,22 @@ HASH_LAST_COMMIT=`git rev-parse --short HEAD`
 LAST_TAG=`git describe --tags --always`
 echo $HASH_LAST_COMMIT
 echo $LAST_TAG
-# git clean -fd
+
+if [[ $LAST_TAG == *$HASH_LAST_COMMIT* ]]
+then
+  echo "It's there!";
+else
+  echo "It's not there!";
+fi
+
+git clean -fd
+
 # zip -x *.git* -r "${APP_NAME}-${APP_VERSION}.zip" .
+
 # aws elasticbeanstalk delete-application-version --application-name "${APP_NAME}" --version-label "${APP_VERSION}"  --delete-source-bundle
+
 # aws s3 cp ${APP_NAME}-${APP_VERSION}.zip s3://${S3_BUCKET}/${APP_NAME}-${APP_VERSION}.zip
+
 # aws elasticbeanstalk create-application-version --application-name "${APP_NAME}" --version-label "${APP_VERSION}" --source-bundle S3Bucket="${S3_BUCKET}",S3Key="${APP_NAME}-${APP_VERSION}.zip"
+
 # aws elasticbeanstalk update-environment --environment-name "${ENV_NAME}" --version-label "${APP_VERSION}"
