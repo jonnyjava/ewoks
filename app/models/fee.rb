08c49eeb4d3b0246667a5463d30fc5888ed51198
GpoNsU <simpons@gmail.com>
2014-08-26 15:20:57 +0200

    Deleted the traces of fees.

--- app/models/fee.rb
+++
@@ -1,9 +0,0 @@
-class Fee < ActiveRecord::Base
-  belongs_to :garage
-  validates :name, presence: true
-  validates :price, presence: true
-
-  def tyre_fee
-    TyreFee.where(fee_id: id).first
-  end
-end
