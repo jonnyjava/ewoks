6f6bd3e2ff543c1572c322b445929a383ced615c
GpoNsU <simpons@gmail.com>
2014-08-29 14:45:17 +0200

    Added notifications to global.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :set_locale
+  before_action :set_notifications
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -27,5 +28,10 @@ class ApplicationController < ActionController::Base
   def extract_locale_from_accept_language_header
     language = request.env['HTTP_ACCEPT_LANGUAGE']
     language.scan(/^[a-z]{2}/).first if language
+  end
+
+  def set_notifications
+    return unless current_user.owner?
+    @notifications = current_user.garage.decorate.notifications
   end
 end
