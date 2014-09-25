6098c7017723383c913b18c65a4f958d71db3d62
Andrea <jonnyjava.net@gmail.com>
2014-09-25 15:27:31 +0200

    testing tyre fees scopes

--- spec/models/tyre_fee_spec.rb
+++ spec/models/tyre_fee_spec.rb
@@ -5,4 +5,28 @@ describe TyreFee do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:price) }
   it { should have_db_column(:price).with_options(precision: 4, scale: 2) }
+
+  it 'should return only tyre fees of the choosen vehicle type' do
+    TyreFee::VEHICLES.each do |vehicle|
+      FactoryGirl.create(:tyre_fee, vehicle_type: TyreFee::VEHICLE_TYPE.key(vehicle))
+    end
+    random_vehicle = TyreFee::VEHICLE_TYPE.to_a.sample
+    vehicle_id = random_vehicle[0]
+    vehicle_type = random_vehicle[1]
+    fees = TyreFee.by_vehicle(vehicle_type)
+    fees.count.should be(1)
+    fees.first.vehicle_type.should be(vehicle_id)
+  end
+
+  it 'should return only tyre fees of the choosen rim' do
+    TyreFee::RIMS.each do |rim|
+      FactoryGirl.create(:tyre_fee, rim_type: TyreFee::RIM_TYPE.key(rim))
+    end
+    random_rim = TyreFee::RIM_TYPE.to_a.sample
+    rim_id = random_rim[0]
+    rim_type = random_rim[1]
+    fees = TyreFee.by_rim(rim_type)
+    fees.count.should be(1)
+    fees.first.rim_type.should be(rim_id)
+  end
 end
