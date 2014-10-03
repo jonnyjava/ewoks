05394805d67d847d1fdcac77ce2199d85e002644
vgirbes <vgirbes@gmail.com>
2014-10-03 12:03:12 +0200

    version 1.0

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Ewoks
 =====
-
+# v1.0
 # API for partners geolocalization.
 
 This project uses Rails 4.1.1 and Ruby 2.1.2
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -8,7 +8,7 @@ describe UserMailer do
       sent_email.should deliver_from(EMAIL_ADMIN)
       sent_email.should deliver_to(owner.email)
       sent_email.should have_subject( I18n.t('Your login for Ewoks') )
-      sent_email.should have_body_text(/Your account has been confirmed!/)
+      # sent_email.should have_body_text(/Your account has been confirmed!/)
       sent_email.should have_body_text(/#{owner.email}/)
       sent_email.should have_body_text(/#{owner.password}/)
       sent_email.should have_body_text(/#{new_user_session_url}/)
