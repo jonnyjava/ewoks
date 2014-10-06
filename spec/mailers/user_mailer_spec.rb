20a59075efb5f50e3982506fc2420ce22e44f579
Andrea <jonnyjava.net@gmail.com>
2014-10-06 17:08:53 +0200

    fix broken test

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -13,7 +13,7 @@ describe UserMailer do
       sent_email.should deliver_from(EMAIL_ADMIN)
       sent_email.should deliver_to(@owner.email)
       sent_email.should have_subject( I18n.t('Your login for Ewoks') )
-      sent_email.should have_body_text(/Your account has been confirmed!/)
+      sent_email.should have_body_text(I18n.t('Your account has been confirmed!'))
       sent_email.should have_body_text(/#{@owner.email}/)
       sent_email.should have_body_text(/#{@owner.password}/)
       sent_email.should have_body_text(/#{new_user_session_url}/)
@@ -25,7 +25,7 @@ describe UserMailer do
      sent_email = UserMailer.send_changed_status_notification(@owner)
      sent_email.should deliver_from(EMAIL_ADMIN)
      sent_email.should deliver_to(@owner.email)
-     sent_email.should have_subject( I18n.t('Your account has been changed') )
+     sent_email.should have_subject( I18n.t('Your account has been changed'))
      sent_email.should have_body_text(I18n.t('Your account has been activated'))
      sent_email.should have_body_text(/#{new_user_session_url}/)
    end
