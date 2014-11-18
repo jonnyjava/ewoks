bb8e5bc40dd96793c4c4643ac9fb1c3bcc9a2273
Andrea <jonnyjava.net@gmail.com>
2014-11-18 16:10:09 +0100

    fix public email broken test

--- spec/mailers/public_form_mailer_spec.rb
+++ spec/mailers/public_form_mailer_spec.rb
@@ -7,9 +7,9 @@ describe PublicFormMailer do
       sent_email = PublicFormMailer.signup_confirmation(garage)
       sent_email.should deliver_from(EMAIL_ADMIN)
       sent_email.should deliver_to(garage.email)
-      sent_email.should have_body_text(/Welcome to Ewoks/)
+      sent_email.should have_body_text(I18n.t('Welcome to Ewoks'))
       sent_email.should have_body_text(/#{garage.signup_verification_token}/)
-      sent_email.should have_subject( I18n.t('Welcome to Ewoks') )
+      sent_email.should have_subject(I18n.t('Welcome to Ewoks'))
     end
   end
 end
