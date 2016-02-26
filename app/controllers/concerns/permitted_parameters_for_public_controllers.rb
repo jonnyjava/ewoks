5bf6ba19ed31133b7db671cf60332fec3b6a58db
jonnyjava <jonnyjava.net@gmail.com>
2016-02-26 16:46:42 +0100

    Applying Code climate feedback

--- app/controllers/concerns/permitted_parameters_for_public_controllers.rb
+++ app/controllers/concerns/permitted_parameters_for_public_controllers.rb
@@ -1,12 +1,9 @@
 module PermittedParametersForPublicControllers
+  include PermittedParametersForTimetable
   extend ActiveSupport::Concern
 
   def garage_params
     params.require(:garage).permit(:name, :owner, :country, :street, :zip, :province, :city, :email, :phone, :mobile, :fax, :latitude, :longitude, :tax_id, :website)
-  end
-
-  def timetable_params
-    params.require(:timetable).permit(:garage_id, :mon_morning_open, :mon_morning_close, :mon_afternoon_open, :mon_afternoon_close, :tue_morning_open, :tue_morning_close, :tue_afternoon_open, :tue_afternoon_close, :wed_morning_open, :wed_morning_close, :wed_afternoon_open, :wed_afternoon_close, :thu_morning_open, :thu_morning_close, :thu_afternoon_open, :thu_afternoon_close, :fri_morning_open, :fri_morning_close, :fri_afternoon_open, :fri_afternoon_close, :sat_morning_open, :sat_morning_close, :sat_afternoon_open, :sat_afternoon_close, :sun_morning_open, :sun_morning_close, :sun_afternoon_open, :sun_afternoon_close)
   end
 
   def holiday_params
--- app/decorators/garage_decorator.rb
+++ app/decorators/garage_decorator.rb
@@ -9,24 +9,40 @@ class GarageDecorator < ApplicationDecorator
   def notifications
     items = []
 
-    items << h.link_to("/garages/#{object.id}/tyre_fees") do
-      h.concat h.content_tag(:i, nil, class: 'ion ion-pricetags danger')
-      h.concat h.t('Add rates tires')
-    end if object.tyre_fees.blank?
-
-    items << h.link_to("/garages/#{object.id}/holidays") do
-      h.concat h.content_tag(:i, nil, class: 'ion ion-calendar warning')
-      h.concat h.t('Indicate vacation days')
-    end if object.holidays.blank?
-
-    items << h.link_to(
-      "/garages/#{object.id}/timetables/#{object.timetable.try(:id)}/edit") do
-      h.concat h.content_tag(:i, nil, class: 'ion ion-clock danger')
-      h.concat h.t('Define the opening hours')
-    end if object.timetable.blank?
+    add_missing_tyre_fee_notification(items) unless object.tyre_fees.present?
+    add_missing_holidays_notification(items) unless object.holidays.present?
+    add_missing_timetable_notification(items) unless object.timetable.present?
 
     h.content_tag :ul, class: 'menu' do
       items.map { |item| h.concat(h.content_tag(:li, item)) }
     end if items.length > 0
   end
+
+  private
+
+  def add_missing_tyre_fee_notification(items)
+    items << h.link_to("/garages/#{object.id}/tyre_fees") do
+      h.concat h.content_tag(:i, nil, class: 'ion ion-pricetags danger')
+      h.concat h.t('Add rates tires')
+    end
+    items
+  end
+
+  def add_missing_holidays_notification(items)
+    items << h.link_to("/garages/#{object.id}/holidays") do
+      h.concat h.content_tag(:i, nil, class: 'ion ion-calendar warning')
+      h.concat h.t('Indicate vacation days')
+    end
+    items
+  end
+
+  def add_missing_timetable_notification(items)
+    items << h.link_to(
+      "/garages/#{object.id}/timetables/#{object.timetable.try(:id)}/edit") do
+      h.concat h.content_tag(:i, nil, class: 'ion ion-clock danger')
+      h.concat h.t('Define the opening hours')
+    end
+    items
+  end
+
 end
