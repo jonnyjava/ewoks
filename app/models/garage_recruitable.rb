f5cab4b8beea0258e7b24ce4ce32815402e824c8
jonnyjava <jonnyjava.net@gmail.com>
2016-03-04 12:35:12 +0100

    email is mandatory

--- app/models/garage_recruitable.rb
+++ app/models/garage_recruitable.rb
@@ -1,7 +1,7 @@
 class GarageRecruitable < ActiveRecord::Base
   enum status: [ :recruitable, :recruited, :has_bounced_us, :waiting_for_answer ]
 
-  validates :name, :tax_id, presence: true
+  validates :name, :email, :tax_id, presence: true
   validates :tax_id, uniqueness: true
   scope :filter_by, ->(param, value) { where("lower(#{param}) LIKE ?", "%#{value.downcase}%") if method_defined?(param) && value && !(columns_hash[param].type == :integer) }
   scope :by_status, ->(status) { where(status: statuses[status]) if status }
--- spec/models/garage_recruitable_spec.rb
+++ spec/models/garage_recruitable_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe GarageRecruitable do
   it { is_expected.to validate_uniqueness_of(:tax_id) }
   it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_presence_of(:tax_id) }
 
   describe "scopes" do
