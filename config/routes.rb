cc11ef971cf14aad79cb0816dcde1dddfa94d95f
jonnyjava <jonnyjava.net@gmail.com>
2016-03-16 00:03:50 +0100

    fixing error controller rendering route

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
     post '/wizard_create_fee/:garage_id', to: 'wizard#create_fee', as: 'wizard_create_fee'
 
     resources :garages do
-      resources :tyre_fees
+      resources :tyre_fees, except: :show
       resources :holidays
       resources :timetables
     end
@@ -46,9 +46,9 @@ Rails.application.routes.draw do
     patch 'users/:id/regenerate_auth_token', to: 'users#regenerate_auth_token', as: 'regenerate_auth_token'
     delete 'garages/:id/destroy_logo', to: 'garages#destroy_logo', as: 'destroy_logo'
 
-    match '/404' => 'errors#error_404', via: :all, as: 'error_404'
-    match '/422' => 'errors#error_422', via: :all, as: 'error_422'
-    match '/500' => 'errors#error_500', via: :all, as: 'error_500'
+    match 'public/404' => 'errors#error_404', via: :all, as: 'error_404'
+    match 'public/422' => 'errors#error_422', via: :all, as: 'error_422'
+    match 'public/500' => 'errors#error_500', via: :all, as: 'error_500'
     match '*path', to: 'errors#error_404', via: :all
   end
 end
