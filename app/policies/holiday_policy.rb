0c1218f48905933f027b5b8f61e17d03a09d034a
jonnyjava <jonnyjava.net@gmail.com>
2016-03-21 12:41:29 +0100

    DRYing garage attributes policies

--- app/policies/holiday_policy.rb
+++ app/policies/holiday_policy.rb
@@ -3,7 +3,7 @@ class HolidayPolicy < ApplicationPolicy
 
   def initialize(user, holiday)
     @user = user
-    @holiday = holiday
+    @authorizable = holiday
   end
 
   def index?
@@ -13,27 +13,4 @@ class HolidayPolicy < ApplicationPolicy
   def show?
     @user.admin? || belong_to_country_manager_country? || belong_to_owner?
   end
-
-  private
-    def is_its_owner?
-      return true if @holiday.blank?
-      return @holiday.garage.owner_id == @user.id if @holiday.try(:garage)
-      garages = @holiday.pluck(:garage_id).uniq
-      (garages.size == 1) && (garages.first == @user.garage.id)
-    end
-
-    def its_garage_country_is_country_manager_country?
-      @user.country == @holiday.garage.country
-    end
-
-    def all_belong_to_country_manager_country?
-      belong_to_country_manager_country? || all_garages_country_eq_country_manager_country?
-    end
-
-    def all_garages_country_eq_country_manager_country?
-      return false unless @holiday.try(:all?)
-      return false unless @user.country_manager?
-      countries = @holiday.map(&:garage).map(&:country).uniq
-      countries.size == 1 && countries.first == @user.country
-    end
 end
