9a83e2945c71e54d037e97637b0538caf8a282bb
jonnyjava <jonnyjava.net@gmail.com>
2016-03-31 15:56:19 +0200

    solving warning deprecations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,12 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  #Active Record suppresses errors raised within `after_rollback`/`after_commit` callbacks and only print them to the logs.
+  #In the next version, these errors will no longer be suppressed.
+  #Instead, the errors will propagate normally just like in other Active Record callbacks.
+  #You can opt into the new behavior and remove this warning by setting:
+  config.active_record.raise_in_transactional_callbacks = true
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
