91cac41960cfcd57a915adcef93f468e46115e41
andrea <jonnyjava.net@gmail.com>
2016-04-12 23:23:37 +0200

    Signup mail token bug (#16)
    * solving warning deprecations
    
    * deleting useless empty script file and its references
    
    * fixing bug

--- spec/routing/garages_routing_spec.rb
+++ spec/routing/garages_routing_spec.rb
@@ -6,7 +6,7 @@ describe GaragesController do
     it { expect(get: '/garages/new').to route_to('garages#new') }
     it { expect(get: '/garages/1').to route_to('garages#show', id: '1') }
     it { expect(get: '/garages/1/edit').to route_to('garages#edit', id: '1') }
-    it { expect(get: '/garages/signup_verification/1').to route_to('garages#signup_verification', token: '1') }
+    it { expect(get: '/garages/signup_verification').to route_to('garages#signup_verification') }
     it { expect(post: '/garages').to route_to('garages#create') }
     it { expect(put: '/garages/1').to route_to('garages#update', id: '1') }
     it { expect(delete: '/garages/1').to route_to('garages#destroy', id: '1') }
